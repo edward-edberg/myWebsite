34b47b502c516fb95b8f49f773b2467fbb9d5efb
Edward Edberg <edward.edberg.halim@gmail.com>
2020-10-15 06:39:07 +0700

    initial commit for my web

---
+++ main.js
@@ -0,0 +1,46 @@
+// alert("GO");
+// alert('ho ho ho');
+
+// $('#publications').click(function() {
+//     alert('ho ho ho');
+// });
+// $(".left").on("click",function() {
+//     alert("GO");
+
+// $(".contact").hide();
+
+$("#home").on("click", function(){
+
+    var win = window.open('index.html', '_self');
+        if (win) {
+            //Browser has allowed it to be opened
+            win.focus();
+        } else {
+            //Browser has blocked it
+            alert('Please allow popups for this website');
+        }
+});
+
+$("#publications").on("click", function(){
+
+    var win = window.open('pubs.html', '_self');
+        if (win) {
+            //Browser has allowed it to be opened
+            win.focus();
+        } else {
+            //Browser has blocked it
+            alert('Please allow popups for this website');
+        }
+});
+
+
+// $(".form-check-inline").on("click", function(){
+//     $(".other-option").fadeOut();
+//     $('#other_text').val('')
+//     $('#category_names').val('')
+//  });
+ 
+//  $(".other-policy").on("click", function(){
+//     $(".other-option").fadeIn();
+//     $("#other_policy").attr("checked", true);
+//  });
